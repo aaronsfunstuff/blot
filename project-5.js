951837691e6a8d377604820c1af38a1181ff248b
aaronsfunstuff <135081747+aaronsfunstuff@users.noreply.github.com>
2024-08-08 15:27:35 -0700

    Add files via upload

---
+++ project-5.js
@@ -0,0 +1,108 @@
+const width = 125;
+const height = 125;
+
+setDocDimensions(width, height);
+
+const finalLines = [];
+
+const appleOutline = [
+  [62.5, 100],
+  [85, 80],
+  [100, 50],
+  [85, 20],
+  [40, 20],
+  [25, 50],
+  [40, 80],
+  [62.5, 100]
+];
+
+const stem = [
+  [58, 100],
+  [62, 115],
+  [67, 100]
+];
+
+const stemShading = [
+  [60, 105],
+  [64, 110],
+  [66, 105]
+];
+
+const leftLeaf = [
+  [45, 85],
+  [30, 105],
+  [50, 100],
+  [45, 85]
+];
+
+const leftLeafVeins = [
+  [40, 95],
+  [35, 90],
+  [45, 85],
+  [30, 95],
+  [45, 100]
+];
+
+const rightLeaf = [
+  [80, 85],
+  [95, 105],
+  [75, 100],
+  [80, 85]
+];
+
+const rightLeafVeins = [
+  [85, 95],
+  [90, 90],
+  [80, 85],
+  [95, 95],
+  [80, 100]
+];
+
+const leftEye = [
+  [50, 55],
+  [45, 50],
+  [55, 50],
+  [55, 60],
+  [45, 60],
+  [45, 50]
+];
+
+const rightEye = [
+  [75, 55],
+  [70, 50],
+  [80, 50],
+  [80, 60],
+  [70, 60],
+  [70, 50]
+];
+
+function smile() {
+  const smileCenterX = 62.5;
+  const smileCenterY = 40;
+  const radius = 10;
+  const startAngle = 0;
+  const endAngle = Math.PI;
+  const smilePoints = [];
+
+  for (let angle = startAngle; angle <= endAngle; angle += 0.1) {
+    const x = smileCenterX + radius * Math.cos(angle);
+    const y = smileCenterY - radius * Math.sin(angle);
+    smilePoints.push([x, y]);
+  }
+
+  finalLines.push(smilePoints);
+}
+
+smile();
+
+finalLines.push(appleOutline);
+finalLines.push(stem);
+finalLines.push(stemShading);
+finalLines.push(leftLeaf);
+finalLines.push(leftLeafVeins);
+finalLines.push(rightLeaf);
+finalLines.push(rightLeafVeins);
+finalLines.push(leftEye);
+finalLines.push(rightEye);
+
+drawLines(finalLines);
